e60cd5c37f49d8acb27b2f47a9829845882be21e
kase-0 <208857206+FernandoSerrano900@users.noreply.github.com>
2025-08-14 12:35:51 -0600

    ✨ feat: Added new functions

--- clase_0.cc
+++ clase_0.cc
@@ -1,11 +1,37 @@
 #include <iostream>
 
+#include <cmath>
 
-
-int main(){
-
-std::cout << "Mi primer snippet";
-
-return 0;
+int CalcularSuma(int x, int y)
+{
+    return x + y;
 }
 
+float CalcularAreaCirculo(float radio)
+{
+    float area;
+    const float kPi= 3.1416;
+
+    area =  kPi * std::pow(radio, 2);
+
+    return round (area*100) / 100;
+}
+
+int main()
+{
+    float radio;
+
+    float area;
+
+    int resultado = CalcularSuma(5, 10);
+    std::cout << "La suma es: " << resultado << std::endl;
+
+    std::cout << "Ingrese el radio del circulo" << std::endl;
+    std::cin >> radio;
+
+    area = CalcularAreaCirculo(radio);
+
+    std::cout << "El area del circulo es: " << area << std::endl;
+
+    return 0;
+}
